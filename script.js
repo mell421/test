ada96f6884c21ee0aaea7b76787dd0220d46a0cb
mell421 <43302304+mell421@users.noreply.github.com>
2020-11-01 00:37:26 +0100

    ajout

---
+++ script.js
@@ -0,0 +1,17 @@
+var nbConclu=0
+var nbConcluBis=0
+var nbSem=0
+var nbMois=0
+
+function ajoutConclu (){
+    return this.nbConclu += 1
+}
+function ajoutConcluBis (){
+    return this.nbConcluBis += 1
+}
+function ajoutSem (){
+    return this.nbSem += 1
+}
+function ajoutMois (){
+    return this.nbMois += 1
+}
