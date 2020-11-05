1bea97dd7c251dfeefa62bd7020861b50cd11995
mell421 <43302304+mell421@users.noreply.github.com>
2020-11-05 18:30:52 +0100

    index.html, tab

--- script.js
+++ script.js
@@ -61,3 +61,48 @@ function ajoutMois (){
     return this.nbMois += 1
 }
 
+
+// 
+
+google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'a');
+      data.addColumn('number', 'r');
+      data.addColumn('number', 'm');
+      data.addColumn('number', 's');
+
+      data.addRows([
+        [1,  37.8, 80.8, 41.8, 41.8],
+        [2,  30.9, 69.5, 32.4, 41.8],
+        [3,  25.4,   57, 25.7, 41.8],
+        [4,  11.7, 18.8, 10.5, 41.8],
+        [5,  11.9, 17.6, 10.4, 41.8],
+        [6,   8.8, 13.6,  7.7, 41.8],
+        [7,   7.6, 12.3,  9.6, 41.8],
+        [8,  12.3, 29.2, 10.6, 41.8],
+        [9,  16.9, 42.9, 14.8, 41.8],
+        [10, 12.8, 30.9, 11.6, 41.8],
+        [11,  5.3,  7.9,  4.7, 41.8],
+        [12,  6.6,  8.4,  5.2, 41.8],
+        [13,  4.8,  6.3,  3.6, 41.8],
+        [14,  4.2,  6.2,  3.4, 41.8]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'taux de ARMS',
+          subtitle: ''
+        },
+        width: 900,
+        height: 500
+      };
+
+      var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
