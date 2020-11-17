3760b13427e34919def5028fa06ffa81b415bdb2
mell421 <43302304+mell421@users.noreply.github.com>
2020-11-17 02:01:20 +0100

    UPDATE

--- sommaire.js
+++ sommaire.js
@@ -1,27 +1,57 @@
-document.write("<p>")
-document.write("<h2 class=\"sommaire\">sommaire</h2>")
-document.write("<p class=\"sommaire\">")
-document.write("<a href=\"index.html\">accueil</a>")
-document.write("<a href=\"tab.html\">tableau 2020</a>")
-document.write("<a href=\"tab2021.html\">tableau 2021</a>")
-document.write("<a href=\"tabMonth.html\">tableau mois</a><br><br>")
-document.write("<a href=\"bl/bl.html\">BL</a> ")
-document.write("<a href=\"bl/bl.pdf\" target=\"_blank\">pdf</a>")
-document.write("<a href=\"bl/bl.md\" target=\"_blank\">md</a><br>")
-document.write("<a href=\"apercu21/apercu21.html\">suivi 2021</a>")
-document.write("<a href=\"apercu21/apercu21.pdf\" target=\"_blank\">pdf</a> ")
-document.write("<a href=\"apercu21/apercu21.md\" target=\"_blank\">md</a><br>")
-document.write("<a href=\"apercu/apercu.html\">suivi 2020</a>")
-document.write("<a href=\"apercu/apercu.pdf\" target=\"_blank\">pdf</a> ")
-document.write("<a href=\"apercu/apercu.md\" target=\"_blank\">md</a><br>")
-document.write("<a href=\"apercu19/apercu2019.html\">suivi 2019</a> ")
-document.write("<a href=\"apercu19/apercu2019.pdf\" target=\"_blank\">pdf</a> ")
-document.write("<a href=\"apercu19/apercu2019.md\" target=\"_blank\">md</a><br>")
-document.write("<a href=\"apercuTer/apercuTer.html\">suivi terminé</a> ")
-document.write("<a href=\"apercuTer/apercuTer.pdf\" target=\"_blank\">pdf</a> ")
-document.write("<a href=\"apercuTer/apercuTer.md\" target=\"_blank\">md</a>")
-document.write("</p></p>")
-document.write("")
+
+document.write(`
+<p class="sommaire">
+
+
+<table class="sommaire" style="font-size: 15px;">
+        <tr class="titre">
+            <td colspan="4">SOMMAIRE</td>
+        </tr>
+        <tr>
+            <td colspan="4"> - </td>
+        </tr>
+        <tr>
+            <td><a href="index.html">accueil</a></td>
+            <td><a href="tab.html">tableau 2020</a></td>
+            <td><a href="tab2021.html">tableau 2021</a></td>
+            <td><a href="tabMonth.html">tableau mois</a></td>
+        </tr>
+        <tr class="titre">
+        <td colspan="4">pages</td>
+        </tr>
+        <tr>
+            <td><a href="bl/bl.html">BL</a></td>
+            <td><a href="bl/bl.pdf" target="_blank">pdf</a></td>
+            <td><a href="bl/bl.md" target="_blank">md</a></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><a href="apercu21/apercu21.html">suivi 2021</a></td>
+            <td><a href="apercu21/apercu21.pdf" target="_blank">pdf</a></td>
+            <td><a href="apercu21/apercu21.md" target="_blank">md</a></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><a href="apercu/apercu.html">suivi 2020</a></td>
+            <td><a href="apercu/apercu.pdf" target="_blank">pdf</a></td>
+            <td><a href="apercu/apercu.md" target="_blank">md</a></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><a href="apercu19/apercu2019.html">suivi 2019</a></td>
+            <td><a href="apercu19/apercu2019.pdf" target="_blank">pdf</a></td>
+            <td><a href="apercu19/apercu2019.md" target="_blank">md</a></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><a href="apercuTer/apercuTer.html">suivi terminé</a></td>
+            <td><a href="apercuTer/apercuTer.pdf" target="_blank">pdf</a></td>
+            <td><a href="apercuTer/apercuTer.md" target="_blank">md</a></td>
+            <td></td>
+        </tr>
+      </table>
+</p>
+`)
 
   
   
