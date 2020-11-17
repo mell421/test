588736d5f8818e8c2f14b419869341cb9a37e48b
mell421 <43302304+mell421@users.noreply.github.com>
2020-11-17 01:09:44 +0100

    vrai menu

---
+++ sommaire.js
@@ -0,0 +1,30 @@
+document.write("<p>")
+document.write("<h2 class=\"sommaire\">sommaire</h2>")
+document.write("<p class=\"sommaire\">")
+document.write("<a href=\"index.html\">accueil</a>")
+document.write("<a href=\"tab.html\">tableau 2020</a>")
+document.write("<a href=\"tab2021.html\">tableau 2021</a>")
+document.write("<a href=\"tabMonth.html\">tableau mois</a><br><br>")
+document.write("<a href=\"bl/bl.html\">BL</a> ")
+document.write("<a href=\"bl/bl.pdf\" target=\"_blank\">pdf</a>")
+document.write("<a href=\"bl/bl.md\" target=\"_blank\">md</a><br>")
+document.write("<a href=\"apercu21/apercu21.html\">suivi 2021</a>")
+document.write("<a href=\"apercu21/apercu21.pdf\" target=\"_blank\">pdf</a> ")
+document.write("<a href=\"apercu21/apercu21.md\" target=\"_blank\">md</a><br>")
+document.write("<a href=\"apercu/apercu.html\">suivi 2020</a>")
+document.write("<a href=\"apercu/apercu.pdf\" target=\"_blank\">pdf</a> ")
+document.write("<a href=\"apercu/apercu.md\" target=\"_blank\">md</a><br>")
+document.write("<a href=\"apercu19/apercu2019.html\">suivi 2019</a> ")
+document.write("<a href=\"apercu19/apercu2019.pdf\" target=\"_blank\">pdf</a> ")
+document.write("<a href=\"apercu19/apercu2019.md\" target=\"_blank\">md</a><br>")
+document.write("<a href=\"apercuTer/apercuTer.html\">suivi terminé</a> ")
+document.write("<a href=\"apercuTer/apercuTer.pdf\" target=\"_blank\">pdf</a> ")
+document.write("<a href=\"apercuTer/apercuTer.md\" target=\"_blank\">md</a>")
+document.write("</p></p>")
+document.write("")
+
+  
+  
+  
+
+  
