550b04748078cbc67fc49b71b7765705056c3755
mell421 <43302304+mell421@users.noreply.github.com>
2020-11-26 01:31:16 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -35,7 +35,7 @@ document.write(`
   <li><a href="#">Item 5</a>
   <ul>
       <li><a href="mesSites/sites.html">Sous-item 1</a></li>
-      <li><a href="#">Sous-item 2</a></li>
+      <li><a href="increment.html">increment</a></li>
       <li><a href="#">Sous-item 3</a></li>
     </ul>
   </li>
