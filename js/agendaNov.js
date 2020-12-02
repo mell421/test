d28a3eb278b5fa9e501722d53a8b746966e26400
mell421 <43302304+mell421@users.noreply.github.com>
2020-12-02 17:02:42 +0100

    add sem 30/11 month 1/12 conclu 27/11 - 1/12

--- js/agendaNov.js
+++ js/agendaNov.js
@@ -1,3 +1,50 @@
 document.write(`
-
+<h2>novembre</h2>
+<p>
+<table class="" style="font-size: 13px; width:100%;">
+  <tr class="titre">
+      <td>lun </td><td>mar </td><td>mer </td><td>jeu </td><td>ven </td><td>sam </td><td>dim </td>
+  </tr>
+  <tr>
+      <td colspan="6"></td>
+      <td>1✔️<br>boys lockdown 103</td>
+  </tr>
+  <tr>
+      <td>2✔️</td><td>3✔️</td><td>4✔️</td>
+      <td>5✔️<br>cherry magic 105<br>ICSYV 105<br>ITSAY 103<br>ben x jim 104</td>
+      <td>6✔️<br><span class="fin">DR NL 108</span></td>
+      <td>7✔️</td>
+      <td>8✔️<br>boys lockdown 104</td>
+  </tr>
+  <tr>
+      <td>9✔️</span></td>
+      <td>10✔️<br>TGDO 402<br><span class="debut">gen y 101</span></td>
+      <td>11✔️</td>
+      <td>12✔️<br>ICSYV 106<br>oh mando 102<br>ITSAY 104<br>ben x jim 105</td>
+      <td>13✔️</td><td>14✔️</td><td>15✔️</td>
+  </tr>
+  <tr>
+      <td>16✔️</td>
+      <td>17✔️<br>TGDO 403<br>gen y 102</td>
+      <td>18✔️</td>
+      <td>19✔️<br>ICSYV 107<br>cherry magic 107<br>ITSAY 105<br>ben x jim 106</td>
+      <td>20✔️<br>tharnType 203</td>
+      <td>21✔️<br>friend forever 110</td>
+      <td>22✔️<br>boys lockdown 105</td>
+  </tr>
+  <tr>
+      <td>23✔️</td>
+      <td>24✔️<br>TGDO 404<br>gen y 103</td>
+      <td>25✔️</td>
+      <td>26✔️<br>oh mando 104<br>cherry magic 108<br><span class="fin">ben x jim 107</span><br>TV GER 1015</td>
+      <td>27✔️<br>ICSYV 108<br>TV NL 1102<br>thonhon chonlathee 103<br>tharnType 204</td>
+      <td>28✔️<br>friend forever 111</td>
+      <td>29✔️<br><span class="fin">boys lockdown 106</span><br>TV GER 1016</td>
+  </tr>
+  <tr>
+      <td>30✔️<br>lakan 103<br>MOD 102</td>
+      <td colspan="6">ajouter unbag</td>
+  </tr>
+</table>
+<hr>
 `)
