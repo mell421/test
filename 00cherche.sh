235e6146d27d1f596d2cda052aa098c8e79161d3
mell421 <43302304+mell421@users.noreply.github.com>
2020-12-18 16:56:49 +0100

    update

--- 00cherche.sh
+++ 00cherche.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 ./0supprZ.sh
 list=( "Lls" "Ingredients" "The voice germany" "Strictly come dancing" "Gameboys" "Wish you" )
-li0=${list[0]}
-./0chercherSerie.sh $li0
-li1=${list[1]}
-./0chercherSerie.sh $li1
-li2=${list[2]}
-./0chercherSerie.sh $li2
-li3=${list[3]}
-./0chercherSerie.sh $li3
-li4=${list[4]}
-./0chercherSerie.sh $li4
-li5=${list[5]}
-./0chercherSerie.sh $li5
 
+echo " " > logChercher
 
-# for i in "${list[@]}"
-# do
-#     echo "i =" $i > logChercher
-#     exec "./0chercherSerie.sh $i"   
-# done
+for i in "${list[@]}"
+do
+    echo "i =" $i >> logChercher
+    ./0chercherSerie.sh $i
+   
+done
 
+echo " " >> logChercher
+for i in "${list[@]}"
+do
+    echo "i =" $i >> logChercher
+    
+    if [[ -f Z/$i ]]
+    then
+        echo "count : " $(wc Z/$i) >> logChercher
+    else
+        j = "$(echo $i | tr '[:blank:]' ' ' )"
+        # i = $(sed 's/""/" "' $i)
+        echo "j =" $j >> logChercher
+    fi;
+    
+    echo " " >> logChercher
+
+done
 
 
