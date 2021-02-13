9553cfca2843a66efa334fc0ab75e76e5142d353
mell421 <43302304+mell421@users.noreply.github.com>
2021-02-13 22:38:43 +0100

    update

--- 00cherche.sh
+++ 00cherche.sh
@@ -3,19 +3,20 @@
 ./0date.sh
 ./0supprZ.sh
 # >= 10
-list=( "Lls _1" 
-"Unbag _1" "Manner of death _1" "The voice nl _11" "Druck _6" "Rpdru _9" "Happenstance _1" 
-"Sea him _1" "Young sheldon _4" "Mako ventures _1" "Koishite akuma _1" "History2 right or wrong _1" "TharnType _2" "Ghost runner _1" )
+list=( "Lls _1" "Unbag _1" 
+"Manner of death _1" "The voice nl _11" "Mako ventures _1" "Druck _6" "Happenstance _1" "Sea him _1" "The voice be _9" 
+"The voice no _6" "Young sheldon _4" "Name that tune _1" "The masked dancer _1" "You are ma boy _1" "Koishite akuma _1" "Nations brother _1" "History2 right or wrong _1" 
+"Rpdr _13" "The voice uk _10" "TharnType _2" "Dancing on ice uk _13" "Be love _1" "Rpdruk _2" "/9-1-1 lone star _2" )
 
 # entre 4 et 9
-listBis=( "The voice no _6" "The masked dancer _1" "Friend zone _1" "Nations brother _1" 
-"The voice be _9" "Pearl next door _1" 
-"The voice uk _10" "Rpdr _13" 
-"Rpdru _13" "You are ma boy _1" "Capture lover _1" 
-"Dancing on ice uk _13" "Name that tune _1" )
+listBis=( "Rpdru _13" "Pearl next door _1" 
+"The harvest season _1" "Wabi vlog _1" "/9-1-1 _4" 
+"Wabi show _1" "Dancing on ice uk _12" 
+"Skam fr _7" "The untamed _1" "The shipper _1" 
+"Ben x jim _1" )
 
-listAtt=( "Friend forever _1" "My vampire boyfriend _1" "Blood _1" 
-"Pogi lang hanap _1" "Be love _1" "Lakan _1" "From dusk till dawn _2" )
+listAtt=( "Friend forever _1" "My vampire boyfriend _1" "Blood _1" "Pogi lang hanap _1" "Lakan _1" "From dusk till dawn _2" "Rpdru _9" 
+"Ghost runner _1" "Friend zone _1" "Capture lover _1" )
 
 echo " " > logChercher
 # echo "" > 0graph.dat
--- js/lastCommit.js
+++ js/lastCommit.js
@@ -1,3 +1,3 @@
 document.write(`<center>
-Date last modification : Fri Feb 12 03:32:22 2021 +0100
+Date last modification : Sat Feb 13 22:18:05 2021 +0100
   </center>`)
