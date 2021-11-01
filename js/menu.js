df0de3ec83aa53e81e6e6684fa4ac54c813e585a
mell421 <43302304+mell421@users.noreply.github.com>
2021-11-01 20:51:39 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -9,18 +9,14 @@ document.write(`
       <li><a href="wordcloud/allListe.html">allListe</a></li>
       <li><a href="wordcloud/sumup.html">sumup</a></li>
       <li><a href="wordcloud/summary.html">summary</a></li>
-      <li><a href="form.html">...</a></li>
+      <li><a href="form.html"></a></li>
       </ul>
   </li>
-  <li><a href="#">tableaux</a>
+  <li><a href="#">series</a>
     <ul>
       <li><a href="tab2020.html">2020</a></li>
       <li><a href="tab2021.html">2021</a></li>
       <li><a href="tabMonth.html">agenda</a></li>
-    </ul>
-  </li>
-  <li><a href="#">séries</a>
-  <ul>
       <li><a href="apercu19/apercu2019.html">séries ec 2019</a></li>
       <li><a href="apercu/apercu20.html">séries ec 2020</a></li>
       <li><a href="apercu/apercu21.html">séries ec 2021</a></li>
